baef5191555c2486a29570f9eee49bdcad5386b0
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-01-11 23:28:05 +0900

    new folder

---
+++ src/containers/Header.js
@@ -0,0 +1,9 @@
+import {useState, useEffect} from "react";
+
+function Header() {
+    return <div>
+        <h1>HEADER</h1>
+    </div>
+}
+
+export default Header;
---
+++ src/containers/Title.js
@@ -0,0 +1,9 @@
+import {useState, useEffect} from "react";
+
+function Title() {
+    return <div>
+        <h3>TITLE</h3>
+    </div>
+}
+
+export default Title;
