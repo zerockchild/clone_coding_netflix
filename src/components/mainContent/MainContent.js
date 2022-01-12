058afbb1d9295dac18940dfe89968835f879940f
chimco <72544949+nsw1055@users.noreply.github.com>
2022-01-13 08:54:51 +0900

    backup

---
+++ src/components/mainContent/MainContent.js
@@ -0,0 +1,12 @@
+import MainView from "./MainView/MainView";
+
+function MainContent() {
+    return(
+        <div>
+            <MainView/>
+            <div style={{height: "120vh"}}>asd</div>
+        </div>
+    )
+}
+
+export default MainContent
