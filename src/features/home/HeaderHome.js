beadd5fd93cc411b6b832e4f4236ba95f84ea210
장민우 <jmw900@naver.com>
2022-03-08 11:31:47 +0900

    2022/03/08 - mw : Redux Toolkit Process3 - HeaderHome 리팩토링

--- src/features/home/HeaderHome.js
+++ src/features/home/HeaderHome.js
@@ -3,6 +3,7 @@ import { useScroll } from "../../common/hooks/useScroll";
 const HeaderHome = ({bannerMovie}) => {
 
     const {REACT_APP_TMDB_IMAGE_URL} = process.env;
+    const bannerContexts = ["홈", "시리즈", "영화", "NEW! 요즘 대세 콘텐츠", "내가 찜한 콘텐츠"];
     const scrollY = useScroll();
 
     const handlePlay = () => {
@@ -14,11 +15,7 @@ const HeaderHome = ({bannerMovie}) => {
             {/* <!-- nav --> */}
             <div id="nav" className={scrollY <= 100? "nav":"nav nav__black"}>
                 <img className="nav__logo" src="images/netflix-logo.png" alt="" />
-                <a className="banner__button" href="/">홈</a>
-                <a className="banner__button" href="/">시리즈</a>
-                <a className="banner__button" href="/">영화</a>
-                <a className="banner__button" href="/">NEW! 요즘 대세 콘텐츠</a>
-                <a className="banner__button" href="/">내가 찜한 콘텐츠</a>
+                {bannerContexts.map( (context, idx) => <a className="banner__button" href="/" key={idx}>{context}</a>)}
                 <img className="nav__avatar" src="images/netflix-avatar.png" alt="" />
             </div>
 
