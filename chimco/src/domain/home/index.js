1ca3571b40837cbc731014d2ba646e8b55a7c457
chimco <72544949+nsw1055@users.noreply.github.com>
2022-04-24 02:30:34 +0900

    test token

--- chimco/src/domain/home/index.js
+++ chimco/src/domain/home/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 function Home() {
-    const { REACT_APP_TMDB_API_URL,REACT_APP_TMDB_TOP_RATE_MOVIE, REACT_APP_TMDB_LANGUAGE, REACT_APP_TMDB_KEY, REACT_APP_TMDB_OPTIONS, REACT_APP_TMDB_THUMBNAIL_IMAGE_URL } = process.env;
+    const { REACT_APP_TMDB_API_URL,REACT_APP_TMDB_TOP_RATE_MOVIE, REACT_APP_TMDB_KEY, REACT_APP_TMDB_OPTIONS, REACT_APP_TMDB_THUMBNAIL_IMAGE_URL } = process.env;
     const [movieDefault, setMovieDefault]= useState({});
     const [topRateMovie, setTopRateMovie] = useState("");
     const [loading, setLoading] = useState(true);
